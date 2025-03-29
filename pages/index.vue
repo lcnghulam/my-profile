cf6449894c3dad7baec4a0d2caa7048121efe428
lcnghulam <lcnghulam1@gmail.com>
2025-03-29 14:53:58 +0700

    Build my-profile with Next.JS 15

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<script setup>
+definePageMeta({
+layouts: 'MainLayout'
+})
+</script>
+
+<template>
+    <MainLayout>
+        <div class="text-center">
+            <h1 class="text-5xl font-bold tracking-tight">Hi, I'm Ghulam</h1>
+            <p class="mt-4 text-lg text-gray-600 dark:text-gray-400">
+                A passionate developer exploring Nuxt.js & Tailwind CSS.
+            </p>
+        </div>
+    </MainLayout>
+</template>
